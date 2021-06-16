b359374300c2bca32eed3820028daf4a9dec1508
SurabhiBadal <s.badal2012@gmail.com>
2021-06-16 14:21:51 +0530

    Adding dependency and hibernate configuration

---
+++ src/main/java/com/lenovo/HibernateDemo/App.java
@@ -0,0 +1,20 @@
+package com.lenovo.HibernateDemo;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+
+        System.out.println( "Project Started" );
+        Configuration cfg=new Configuration();
+        cfg.configure();
+        SessionFactory factory=cfg.buildSessionFactory();
+        System.out.println(factory);
+    }
+}
